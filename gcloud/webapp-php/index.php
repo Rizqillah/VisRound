fefeb0badef2e83009fc503bbeddb460f8a883ee
Samuel-C1951948 <84013134+Samuel-C1951948@users.noreply.github.com>
2021-06-07 21:50:41 +0700

    Update index.php

--- gcloud/webapp-php/index.php
+++ gcloud/webapp-php/index.php
@@ -5,27 +5,12 @@
     <link rel="stylesheet" href="/assets/style.css">
     <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta charset="utf-8">
-    <script>async function run(){
+    <script>async function klik(){
   document.getElementById("text").innerHTML = "Fatal Accident";
     }
     </script>
     
-    <script>
-        window.addEventListener('load', function(){
-            document.querySelector('input[type="file"]').addEventListener('change',function(){
-                if(this.files && this.files[0]){
-                    var img=document.querySelector('img');
-                    img.src=URL.createObjectURL(this.files[0]);
-                    img.onload=loadedimage;
-                }
-            });
-        });
-        
-        function loadedimage(){
-            alert("Image Successfully Uploaded");
-        }</script>
-        
-    <script>
+        <script>
         window.addEventListener('load', function(){
             document.querySelector('input[type="file"]').addEventListener('change',function(){
                 if(this.files && this.files[0]){
@@ -49,7 +34,7 @@
         <p>Warehouse Accident Detection</p>   
         <img id="img" src="#" width="300px" height="300px">
         <p></p>
-        <input type="file"> <button onclick="run()">Detect</button>
+        <input type="file"> <button onclick="klik()">Detect</button>
         <h3>Image Detected as : <h5 id="text" style="display:inline;"></h5> </h3>
     </div>    
     
